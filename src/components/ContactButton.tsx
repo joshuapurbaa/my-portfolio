25ed75c3ee7396bdb55e18013a29bf25c7654a9b
joshuapurbaa <yoshuapurba06@gmail.com>
2024-12-16 09:44:38 +0700

    refactor: integrate responsive design into ContactButton component

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button/Button";
+import useResponsive from "../hooks/useResponsive";
 
 export interface ContactButtonProps {
   icon: React.ReactNode;
@@ -10,21 +11,25 @@ const ContactButton: React.FC<ContactButtonProps> = ({
   icon,
   text,
   onClick,
-}) => (
-  <Button
-    variant="outlined"
-    color="primary"
-    startIcon={icon}
-    sx={{
-      fontSize: "1rem",
-      padding: "10px 20px",
-      width: "200px",
-      height: "50px",
-    }}
-    onClick={onClick}
-  >
-    {text}
-  </Button>
-);
+}) => {
+  const { isMobile, isTablet } = useResponsive();
+
+  return (
+    <Button
+      variant="outlined"
+      color="primary"
+      startIcon={icon}
+      sx={{
+        fontSize: isMobile ? "0.8rem" : isTablet ? "0.9rem" : "1rem",
+        padding: isMobile ? "8px 16px" : isTablet ? "9px 18px" : "10px 20px",
+        width: isMobile ? "160px" : isTablet ? "180px" : "200px",
+        height: isMobile ? "40px" : isTablet ? "45px" : "50px",
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default ContactButton;
